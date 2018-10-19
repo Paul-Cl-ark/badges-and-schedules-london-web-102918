80eced5d9dc23e9cab02baac8a3c3570fa50adf4
Paul Clark <clark.paul123@gmail.com>
2018-10-19 14:01:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,14 +4,16 @@ end
 
 def batch_badge_creator(attendees)
   badges = []
-  attendees.each do |a|
-    badges.push("Hello, my name is #{a}.")
+  attendees.each do |name|
+    badges.push("Hello, my name is #{name}.")
   end
   badges
 end
 
 def assign_rooms(attendees)
-  "Hello, #{name}! You'll be assinged to room !"
+  room_ass = []
+  attendees.each_with_index do |name, room|
+  "Hello, #{name}! You'll be assinged to room #{room}!"
 end
 
 def printer(attendees)
