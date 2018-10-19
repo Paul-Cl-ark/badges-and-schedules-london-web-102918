76f7fcfdc9be8c6b56781a2619797aaaec528b58
Paul Clark <clark.paul123@gmail.com>
2018-10-19 13:54:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,14 @@
 def badge_maker(name)
   "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(attendees)
+  attendees
+end
+
+def assign_rooms(attendees)
+  "Hello, #{name}! You'll be assinged to room #{room}!"
+end
+
+def printer(attendees)
+  puts 
