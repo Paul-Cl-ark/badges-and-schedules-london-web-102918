14f34b85573261093098d73f3734740c65ebec0a
Paul Clark <clark.paul123@gmail.com>
2018-10-19 13:57:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  badges = []
   attendees.each do |a|
-    "Hello, my name is #{a}."
+    badges.push "Hello, my name is #{a}."
   end
 end
 
